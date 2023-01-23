b60af8e4398a365bbb6d8aca12fc4aa78de01827
Bob van den Hoogen <bobvandenhoogen@hotmail.com>
2023-01-23 13:06:34 +0100

    feat: add stats property and properties for the metrics (#6)
    * feat: add stats property and properties for the metrics
    
    * chore: update readme
    
    Co-authored-by: Bob van den Hoogen <bob@strmprivacy.io>

--- strmprivacy/diagnostics/diagnostics.py
+++ strmprivacy/diagnostics/diagnostics.py
@@ -13,7 +13,8 @@ class PrivacyDiagnostics:
     defaultMetrics = ['k_anonymity', 'l_diversity']
 
     def __init__(self, file: str, sample: int = 0):
-        self.k, self.l, self.t = None, None, None
+        self.k_anonymity, self.l_diversity, self.t_closeness = None, None, None
+        self._k, self._l, self._t = None, None, None
         self.file = file
         self.df = pd.read_csv(file, on_bad_lines='warn')
         if 0 < sample <= len(self.df):
@@ -34,28 +35,28 @@ class PrivacyDiagnostics:
                 else:
                     raise TypeError(f"Cannot infer type of column '{s}'")
         # k-Anonymity
-        self.k = k_anonymity(self.df, qi) if 'k_anonymity' in metrics else None
+        self.k_anonymity = k_anonymity(self.df, qi) if 'k_anonymity' in metrics else None
         # l-Diversity
-        self.l = l_diversity(self.df, qi, sa) if 'l_diversity' in metrics and len(sa) > 0 else None
+        self.l_diversity = l_diversity(self.df, qi, sa) if 'l_diversity' in metrics and len(sa) > 0 else None
         # t-Closeness
-        self.t = t_closeness(self.df, qi, sa, sa_types) if 't_closeness' in metrics and len(sa) > 0 else None
+        self.t_closeness = t_closeness(self.df, qi, sa, sa_types) if 't_closeness' in metrics and len(sa) > 0 else None
 
     def create_report(self, qi: list[str], sa: list[str] = [], sa_types: list[str] = [],
                       metrics=defaultMetrics, path: str = '.'):
-        if ('k_anomyity' in metrics and self.k is None) or \
-                ('l_diversity' in metrics and self.l is None) or \
-                ('t_closeness' in metrics and self.t is None):
+        if ('k_anonymity' in metrics and self.k_anonymity is None) or \
+                ('l_diversity' in metrics and self.l_diversity is None) or \
+                ('t_closeness' in metrics and self.t_closeness is None):
             self.calculate_stats(qi, sa, sa_types, metrics)
 
         with tempfile.TemporaryDirectory() as tmpdir:
             # Plots
-            if self.k is not None:
-                plot_k_anonymity(self.df, self.k, tmpdir)
-            if self.l is not None:
-                plot_l_diversity(self.df, sa, self.l, tmpdir)
+            if self.k_anonymity is not None:
+                plot_k_anonymity(self.df, self.k_anonymity, tmpdir)
+            if self.l_diversity is not None:
+                plot_l_diversity(self.df, sa, self.l_diversity, tmpdir)
 
             # Report
-            Report(tmpdir=tmpdir).create(self.k, self.l, self.t, path)
+            Report(tmpdir=tmpdir).create(self.k_anonymity, self.l_diversity, self.t_closeness, path)
 
     def assert_columns(self, columns: list[str]):
         for column in columns:
@@ -80,6 +81,29 @@ class PrivacyDiagnostics:
         if 't_closeness' in metrics:
             logging.warning("t-Closeness can drastically increase calculation times with larger datasets and number "
                             "of sensitive attributes.")
+
+    @property
+    def stats(self):
+        stats = {}
+        if self.k_anonymity is not None:
+            stats['k'] = self.k
+        if self.l_diversity is not None:
+            stats['l'] = self.l
+        if self.t_closeness is not None:
+            stats['t'] = self.t
+        return stats
+
+    @property
+    def k(self):
+        return min(self.k_anonymity)
+
+    @property
+    def l(self):
+        return {k: min(v) for k, v in self.l_diversity.items()}
+
+    @property
+    def t(self):
+        return self.t_closeness
 
 
 def main(args: dict):
